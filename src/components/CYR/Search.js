d5c363ed93d19c4cee9a7d1f9f5aa8dc11183de3
최유리 <choiyuri@choeyuliui-MacBookAir.local>
2023-10-04 16:03:12 +0900

    10월 04일 test

--- src/components/CYR/Search.js
+++ src/components/CYR/Search.js
@@ -1,244 +1,178 @@
-import React, { useEffect, useRef, useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCartShopping,
-  faHandshakeAngle,
-  faMagnifyingGlass,
-  faMoneyCheckDollar,
-} from "@fortawesome/free-solid-svg-icons";
-import { ko } from "date-fns/esm/locale";
-import { addDays, subDays } from "date-fns";
-import { NavLink, useLocation } from "react-router-dom";
-import axios from "axios";
-import styled from "styled-components";
+import React, { useEffect, useState} from 'react'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
+import {ko} from 'date-fns/esm/locale'
+import {addDays, subDays} from 'date-fns'
+import { NavLink, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
 
-
-const Content = styled.div`
+const Wrapper = styled.div`
   margin-top: 5px;
   width: 100%;
   height: 496px;
   background: url('/images/main-banner.jpg');
   background-size: cover;
 `
-
-
-const ContentWrap = styled.div`
+const SearchContent = styled.div`
   max-width: 1200px;
-  margin: 0 auto;
+  height: 70px;
+  margin: 0 auto ;
   display: flex;
-  transform: translateY(500%);
-`
-
-const Inner = styled.div`
-  height: 75px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-around;
   flex-wrap: nowrap;
   background-color: #fff;
-  border-radius: 100px;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
-`
-
-const Select = styled.select`
-  width: 10%;
-  padding: 2%;
-  font-size: 1.2em;
-  font-weight: bold;
-  border: none;
-  -webkit-appearance:none; //크롬 화살표 없애기
-  appearance: none; //화살표 없애기
-  -moz-appearance: none; //파이어폭스 화살표 없애기
-  text-align: center;
-  &:focus{
-    outline: none;
-    border-radius: 100px;
-    border: 1px solid #2ed090;
+  border-radius: 10px;
+  align-items: center;
+  >input{
+    flex-basis: 30%;
+    height: 100%;
+    font-size: 1.2em;
+    background: none;
   }
+  
 `
-const Option = styled.option`
-  font-size: 1em;
-  border: none;
-  text-align: center;
-
-`
-
 const StyleDate = styled(DatePicker)`
-  width: 250px;
-  height: 65px;
-  font-size: 1.2em;
-  font-weight: bold;
-  border: 0;
-  appearance: none;
-  -moz-appearance: none;
-  text-align: center;
-  &:focus{
-    outline: none;
-    border-radius: 100px;
-    border: 1px solid #2ed090;
-  }
+    color: #666666;
+    height: 100%;
+    border: none;
+    font-size: 1.2em;
+    flex-basis: 40%;
+    background: none;
 `
-
-const Input = styled.input`
-  border: none;
-  width: 40%;
-  padding: 2%;
-  font-size: 1.2em;
-  font-weight: bold;
-  border: 0;
-  appearance: none;
-  -moz-appearance: none;
-  text-align: center;
-  &:focus{
-    outline: none;
-    border-radius: 100px;
-    border: 1px solid #2ed090;
-  }
-`
-
-
-
 const NavLinkWrap = styled.div`
   background-color: #98eecc;
+  color: #fff;
   font-weight: bold;
-  border-radius: 100px;
+  border-radius: 10px;
+  font-size: 18px;
   right: 0;
-  flex-basis: 15%;
-  height: 75%;
-    a{
+  flex-basis: 20%;
+  height: 80%;
+  a{
     display: flex;
     height: 100%;
     align-items: center;
-      p{
-        width: 100%;
-        font-size: 1.7em;
-        text-align: center;
-        font-weight: bold;
-        color: #fff;
-      }
+    p{
+      width: 100%;
+      font-size: 30px;
+      margin: 10px auto;
+      text-align: center;
+      font-weight: 800;
+      color: #fff;
+    }
   }
-`;
-
-
-
-
-
-
-
-
-const Mwrap = styled.div`
-  z-index: 50;
-  background-color: #fff;
-  padding-top: 5px;
-  width: 100%;
-  height: 300px;
-  position: fixed;
-  top: 0;
-  display: flex;
-  justify-content: space-around;
 `
-
-const Mwrapper = styled.div`
-  width: 70%;
-  height: 40px;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: nowrap;
-  border-radius: 10px;
-  align-items: center;
-`
-
-const MinputBox = styled.div`
-  flex-basis: 3/4;
-  border: 1px solid #ddd;
+const Input = styled.input`
+  border: none;
+  right: 30%;
   height: 100%;
+`
+const Select = styled.select`
+  font-size: 24px;
+  font-weight: bold;
+  border: none;
+  color: #666666;
+  flex-basis: 8%;
+  background: none;
+  select:required:invalid{
+    color: #909090;
+  }
+  option{color: #ddd;}
+  option[value=""][disabled] {
+  display:none
+  }
+`
+const Mwrap = styled.div`
+  z-index: 50; background-color: #fff; 
+  padding-top: 5px; width: 100%; height: 300px;
+  position: fixed; top: 0; display: flex; justify-content: space-around;
+`
+const Mwrapper = styled.div`
+   width: 70%; height:40px;
+   display: flex; justify-content: space-between; flex-wrap: nowrap; border-radius: 10px;
+   align-items: center;
+`
+const MinputBox = styled.div`
+  flex-basis: 3/4; 
+  border: 1px solid #ddd; height: 100%;
   border-radius: 5px;
-  > input {
+  >input{
     border: none;
     height: 100%;
   }
 `
-
 const MbuttonBox = styled.div`
   flex-basis: 1/4;
-  border: 1px solid #ddd;
-  height: 100%;
+  border: 1px solid #ddd; height: 100%;
   border-radius: 5px;
-  > button {
+  >button{
     border: 1px solid #ddd;
     background-color: #98eecc;
     color: #fff;
     font-weight: bold;
     border-radius: 5px;
     font-size: 25px;
-    right: 0;
-    width: 100%;
-    height: 100%;
+    right: 0; width: 100%; height: 80%;
   }
 `
 
 // ${scrollPosition > 500 ? "block" : "hidden"}`}
-
 function Search() {
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
   const [alldonm, setAllDonm] = useState([]);
-  const [donm, setDonm] = useState("");
-  const [ischoice, setIsChoice] = useState([null, null]);
+  const [donm, setDonm] = useState("")
+  const [ischoice, setIsChoice] = useState([null,null]);
   const [scrollPosition, setScrollPosition] = useState(0);
   // const [page, setPage] = useState(1);
-  const [Selected, setSelected] = useState("");
-  const [userInput, setUserInput] = useState("");
+  // const [Selected, setSelected] = useState("");
+  const [userInput, setUserInput] = useState('');
   const [optiondonmSelect, setOptionDonmSelect] = useState("");
-
+  
   // const getValue = (e) => {
   //   setUserInput(e.target.value)};
 
-  const updateScroll = () => {
-    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
-  };
 
-  useEffect(() => {
-    window.addEventListener("scroll", updateScroll);
-  }, []);
-  const searched = alldonm.filter((item) => item.facltNm.includes(userInput));
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    };
+    
+    useEffect(() => {
+            window.addEventListener("scroll", updateScroll);
+        }, []);
 
-  useEffect(() => {
-    fetch(
-      "https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=project&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json"
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setDonm(data.response.body.items.item);
+    const searched = alldonm.filter((item) =>
+
+      item.facltNm.includes(userInput)
+    );
+
+
+
+    
+        useEffect(()=>{
+      fetch("https://apis.data.go.kr/B551011/GoCamping/basedList?numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=project&serviceKey=hQ42F%2BSKq2L%2FUrlhNoGxv63elQn7W8CmL22xl6yXuGk%2BMz0zdU%2Frk2CIdCeX5%2BYPmg39K5QBYCeSgUyqtD7Qdg%3D%3D&_type=json")
+      .then((res) =>{return res.json()})
+      .then((data)=> {
+        setDonm(data.response.body.items.item)
         setAllDonm(data.response.body.items.item);
-      });
-  }, []);
-
-  const optionDonm = (e) => {
-    const donmValue = e.target.value;
-    setOptionDonmSelect(donmValue);
-
-    if (donmValue !== "전체") {
-      setIsChoice([1, 0]);
-    } else {
-      setIsChoice([0, 0]);
-    }
-    setUserInput(e.target.value);
-  };
-
-  //   const FilterData = donm && donm.filter(e =>{
-  //   return donm === "전체" || donm === e.doNm
-  //  })
-  const Filterdonm = [...new Set(alldonm && alldonm.map((e) => e.doNm).sort())];
+      }); 
+    },[]);
+    
+    const optionDonm = (e) =>{
+        const donmValue = e.target.value
+        setOptionDonmSelect(donmValue);
+        setUserInput(e.target.value)
+      }
+    
+      //   const FilterData = donm && donm.filter(e =>{
+      //   return donm === "전체" || donm === e.doNm
+      //  })
+       const Filterdonm = [...new Set(alldonm && alldonm.map(e=>e.doNm).sort())];
   return (
     <>
+      
       {/* 모바일 써치+버튼 시작 */}
-      {/* <Mwrap className={scrollPosition > 500 ? ".on" : ""}>
+        {/* <Mwrap className={scrollPosition > 500 ? ".on" : ""}>
           <Mwrapper>
             <MinputBox className='basis-3/4 border h-full rounded'>
               <input type="text" placeholder='검색어를 입력하세요'/>
@@ -252,48 +186,43 @@ function Search() {
             </div>
           </Mwrapper>
         </Mwrap> */}
-      {/* 모바일 써치+버튼 끝 */}
-      {/* 유리써치 */}
-
-     
-      <Content> 
-        <ContentWrap>
-          <Inner>
-          <Select onChange={optionDonm}>
-            <Option value="전체">전체</Option>
-            {Filterdonm.map((e, i) => {
-              return <Option key={i}>{e}</Option>;
-            })}
-          </Select>
-          <StyleDate
-            locale={ko}
-            selectsRange={true}
-            startDate={startDate}
-            endDate={endDate}
-            onChange={(date) => setDateRange(date)}
-            dateFormat="yyyy년 MM월 dd"
-            minDate={subDays(new Date(), 0)}
-            maxDate={addDays(new Date(), 300)}
-            monthsShown={2}
-          />
-          <Input
-            type="text"
-            placeholder="검색어를 입력하세요"
-            onChange={optionDonm}
-          />
-          <NavLinkWrap>
-            <NavLink to="/searchd">
-              {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
-              <p>검색하기</p>
-            </NavLink>
-          </NavLinkWrap>
-          </Inner>
-        </ContentWrap>
-        </Content>
+      {/* 모바일 써치+버튼 끝 */}               
+      {/* 유리써치 시작*/}
+        <Wrapper>
+          <SearchContent>
+            <Select  onChange={optionDonm}>
+              <option value="" disabled selected>어디로 떠나볼까요</option>
+              <option value="전체">전체</option>
+                {
+                   Filterdonm.map((e,i) =>{
+                    return(
+                      <option key={i}>{e}</option>)
+                  })
+                }
+            </Select>
+            <StyleDate
+              locale={ko}
+              selectsRange={true}
+              startDate={startDate} 
+              endDate={endDate} 
+              onChange={(date)=>setDateRange(date)} 
+              dateFormat="MM월 dd일"
+              placeholderText="날짜를 입력하세요"
+              minDate={subDays(new Date(), 0)}
+              maxDate={addDays(new Date(), 300)}
+              monthsShown={2} />
+            <Input type='text' placeholder='검색어를 입력하세요' onChange={optionDonm} />
+            <NavLinkWrap>
+              <NavLink to='/searchd'>
+                <p>검색하기</p>
+              </NavLink>
+            </NavLinkWrap>
+          </SearchContent>
+        </Wrapper>
+        {/* 유리써치 끝*/}
       
-      {/* 유리써치 끝*/}
     </>
-  );
-}
+  )
+  }
 
-export default Search;
+export default Search
